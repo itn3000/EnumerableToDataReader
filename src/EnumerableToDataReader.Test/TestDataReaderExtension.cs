1767c878b96043c8c2129bd0dededae823042dbc
itn3000 <itn3000@gmail.com>
2016-10-18 12:06:04 +0900

    add datareader to dictionary extension method,update version 1.1.0

---
+++ src/EnumerableToDataReader.Test/TestDataReaderExtension.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnumerableToDataReader.Test
+{
+    using Xunit;
+    using System.Reflection;
+    public class TestDataReaderExtension
+    {
+        public void TestToDictionary()
+        {
+            var data = Enumerable.Range(0, 100)
+                .Select(i =>
+                {
+                    return new SampleClass()
+                    {
+                        a = i.ToString()
+                        ,
+                        b = (short)(i & 0xffff)
+                        ,
+                        c = i
+                        ,
+                        d = (byte)(i & 0xff)
+                        ,
+                        e = i
+                        ,
+                        f = i
+                        ,
+                        g = (char)i
+                        ,
+                        h = DateTime.MinValue.AddDays(i)
+                        ,
+                        i = Guid.Empty
+                        ,
+                        j = i
+                        ,
+                        k = Enumerable.Range(0, i).Select(j => (byte)(i & 0xff)).ToArray()
+                    };
+                }).ToArray()
+                ;
+            var properties = typeof(SampleClass).GetTypeInfo().GetProperties().ToDictionary(x => x.Name, x => x);
+            using (var dr = data.AsDataReader())
+            {
+                int cnt = 0;
+                foreach (var x in dr.ToDictionary().Select((dic,i) => new { dic, i }))
+                {
+                    Assert.Equal(properties.Count, x.dic.Count);
+                    foreach(var kv in x.dic)
+                    {
+                        Assert.Equal(properties[kv.Key].GetValue(data[x.i]), kv.Value);
+                    }
+                    cnt++;
+                }
+                Assert.Equal(data.Length, cnt);
+            }
+        }
+    }
+}
---
+++ src/EnumerableToDataReader/DataReaderExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnumerableToDataReader
+{
+    using System.Data;
+    public static class DataReaderExtension
+    {
+        /// <summary>
+        /// convert datareader to dictionary list
+        /// </summary>
+        /// <param name="dr">data source</param>
+        /// <remarks>this function do not enumerate next result,so dr includes multiple results,you must execute multiple times per NextResult()</remarks>
+        /// <returns>string(name)-object(value) key-value dictionary of datareader</returns>
+        public static IEnumerable<IDictionary<string, object>> ToDictionary(this IDataReader dr)
+        {
+            int fieldCount = -1;
+            string[] names = null;
+            while (dr.Read())
+            {
+                if (fieldCount < 0)
+                {
+                    fieldCount = dr.FieldCount;
+                    names = new string[fieldCount];
+                }
+                var dic = new Dictionary<string, object>();
+                for (int i = 0; i < fieldCount; i++)
+                {
+                    if(names[i] == null)
+                    {
+                        names[i] = dr.GetName(i);
+                    }
+                    dic[names[i]] = dr.GetValue(i);
+                }
+                yield return dic;
+            }
+        }
+    }
+}
